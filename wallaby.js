b62f1d4b24bd34e2ea69432586e6925e87779233
Yuri Sim <edaraks@gmail.com>
2025-08-30 11:59:20 -0500

    chore: Comment out specific tests in wallaby config

--- wallaby.js
+++ wallaby.js
@@ -2,5 +2,6 @@ module.exports = function (wallaby) {
   return {
     autoDetect: true,
     tests: ['apps/**/*spec.ts', 'libs/**/*spec.ts', '!**/*-e2e/**/*.spec.ts'],
+    // tests: ['apps/**/*spec.ts', 'libs/**/*spec.ts'],
   };
 };
