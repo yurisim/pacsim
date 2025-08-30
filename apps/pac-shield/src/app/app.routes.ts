c13e113f71b83fdbaeeda6837e06b824c36d7f21
Yuri Sim <edaraks@gmail.com>
2025-08-30 18:12:59 -0500

    feat: Configure Angular Routing

--- apps/pac-shield/src/app/app.routes.ts
+++ apps/pac-shield/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Route } from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Routes = [
+  {
+    path: 'join',
+    loadComponent: () =>
+      import('./features/join/join.component').then((m) => m.JoinComponent),
+  },
+  {
+    path: 'game/:id',
+    loadComponent: () =>
+      import('./features/game/game-board/game-board.component').then((m) => m.GameBoardComponent),
+  },
+  {
+    path: '',
+    redirectTo: 'join',
+    pathMatch: 'full',
+  },
+];
