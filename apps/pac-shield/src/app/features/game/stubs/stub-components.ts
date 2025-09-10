3055589b7a49996e06baa927983be3fa7c228017
Yura Sim <yura.sim@outlook.com>
2025-09-09 19:08:51 -0500

    chore(game): Remove unused MatButtonModule from stub components

--- apps/pac-shield/src/app/features/game/stubs/stub-components.ts
+++ apps/pac-shield/src/app/features/game/stubs/stub-components.ts
@@ -6,7 +6,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 /**
