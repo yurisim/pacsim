4545bd583e5be65a7bee05a57b6de5bf3044cd78
Yuri Sim <edaraks@gmail.com>
2025-09-14 16:46:06 -0500

    refactor: Remove obsolete responsive navigation logic and imports

--- apps/pac-shield/src/app/features/game/game-stats/responsive-nav/responsive-nav.component.ts
+++ apps/pac-shield/src/app/features/game/game-stats/responsive-nav/responsive-nav.component.ts
@@ -1,12 +1,7 @@
 import { Component, inject, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatRippleModule } from '@angular/material/core';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatTabsModule } from '@angular/material/tabs';
 import { ResponsiveNavService, NavItem } from '../responsive-nav.service';
 
@@ -16,12 +11,7 @@ import { ResponsiveNavService, NavItem } from '../responsive-nav.service';
   imports: [
     CommonModule,
     MatIconModule,
-    MatButtonModule,
     MatBadgeModule,
-    MatRippleModule,
-    MatTooltipModule,
-    MatButtonToggleModule,
-    MatSidenavModule,
     MatTabsModule
   ],
   templateUrl: './responsive-nav.component.html',
@@ -29,22 +19,18 @@ import { ResponsiveNavService, NavItem } from '../responsive-nav.service';
 })
 export class ResponsiveNavComponent {
   @Output() tabChange = new EventEmitter<string>();
-  
+
   navService = inject(ResponsiveNavService);
-  
-  navigationMode$ = this.navService.navigationMode$;
+
   activeTab$ = this.navService.activeTab$;
   navItems = this.navService.navItems;
-  
+
   onTabSelect(tabId: string): void {
     this.navService.setActiveTab(tabId);
     this.tabChange.emit(tabId);
-    
+
     // Close side nav when selecting a tab on tablet
     this.navService.closeSideNav();
   }
-  
-  toggleSideNav(): void {
-    this.navService.toggleSideNav();
-  }
+
 }
