ed7a65dc4486eb8ec682486a4f663781747bab62
Yuri Sim <edaraks@gmail.com>
2025-09-14 14:21:52 -0500

    fix: Add envFile to production configuration in project.json fix: Update ESLint config to ignore generated files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: ['**/dist', '**/generated/**'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
