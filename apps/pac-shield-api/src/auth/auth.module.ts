5a0971013fffe95d9df6d89ca9f5066ac9160bc0
Yuri Sim <edaraks@gmail.com>
2025-09-20 12:58:57 -0500

    feat(auth): export JwtModule from AuthModule

--- apps/pac-shield-api/src/auth/auth.module.ts
+++ apps/pac-shield-api/src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtAuthGuard } from '../app/auth/jwt-auth.guard';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtAuthGuard],
-  exports: [AuthService, JwtAuthGuard],
+  exports: [AuthService, JwtAuthGuard, JwtModule],
 })
 export class AuthModule {}
 
