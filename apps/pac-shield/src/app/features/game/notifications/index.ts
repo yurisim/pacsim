1d7d2d6703434a249c5a9dba46fefc7b7b9621f1
Yuri Sim <edaraks@gmail.com>
2025-09-21 06:25:29 -0500

    Chore(client): Remove barrel exports from Angular modules

--- apps/pac-shield/src/app/features/game/notifications/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './allocation-notification-toast/allocation-notification-toast.component';
-export * from './allocation-notification-center/allocation-notification-center.component';
-export * from './allocation-notification-badge/allocation-notification-badge.component';
--- apps/pac-shield/src/app/store/ato/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all ATO state management components
-export * from './ato.state';
-export * from './ato.actions';
-export * from './ato.reducer';
-export * from './ato.selectors';
-export * from './ato.effects';
